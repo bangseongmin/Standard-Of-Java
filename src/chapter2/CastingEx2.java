266ff2c9c9349bc4618cf7b7b110438561f46ef3
daniel <bangseongmin@naver.com>
2024-01-29 19:07:04 +0900

    Chapter 02.  변수 (#2)

---
+++ src/chapter2/CastingEx2.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+/**
+ * 정수간의 형변환
+ * p. 76
+ */
+public class CastingEx2 {
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        byte b = (byte) i;
+        System.out.printf("[int to byte] i=%d -> b=%d\n", i, b);
+
+        i = 300;
+        b = (byte) i;
+        System.out.printf("[int to byte] i=%d -> b=%d\n", i, b);
+
+        b = 10;
+        i = (int) b;
+        System.out.printf("[byte to int] b=%d -> i=%d\n", b, i);
+
+        b = -2;
+        i = (int) b;
+        System.out.printf("[byte to int] b=%d -> i=%d\n", b, i);
+
+        System.out.println("i=" + Integer.toBinaryString(i));
+    }
+}
---
+++ src/chapter2/VarEx2.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+/**
+ * 두 변수의 값 교환하기
+ * p. 24
+ */
+public class VarEx2 {
+
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        int tmp = 0;
+
+        System.out.println("x: " + x + ", y: " + y);
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("x: " + x + ", y: " + y);
+    }
+}
