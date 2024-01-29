9b7d131e68c9f2740a6d07156941bfd1f51d5b38
daniel <bangseongmin@naver.com>
2024-01-29 21:26:57 +0900

    Chapter 04. 반복문

---
+++ src/chapter4/FlowEx23.java
@@ -0,0 +1,16 @@
+package chapter4;
+
+/**
+ * 반복문
+ * p. 168
+ */
+public class FlowEx23 {
+
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " - I can do it.");
+        }
+    }
+}
