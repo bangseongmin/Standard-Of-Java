8a3c63e61c166206da1c6bfdfb41cd9676eced26
daniel <bangseongmin@naver.com>
2024-02-07 01:07:16 +0900

    Chapter 06. 객체지향 프로그램 1

---
+++ src/chapter6/TvTest4.java
@@ -0,0 +1,19 @@
+package chapter6;
+
+public class TvTest4 {
+
+    public static void main(String[] args) {
+        TV[] tvArr = new TV[3];
+
+        // TV객체를 생성해서 TV객체 배열의 각 요소에 저장
+        for (int i = 0; i < tvArr.length; i++) {
+            tvArr[i] = new TV();
+            tvArr[i].channel = i + 10;
+        }
+
+        for(int i = 0; i < tvArr.length; i++) {
+            tvArr[i].channelUp();
+            System.out.printf("tvArr[%d].channel=%d\n", i, tvArr[i].channel);
+        }
+    }
+}
