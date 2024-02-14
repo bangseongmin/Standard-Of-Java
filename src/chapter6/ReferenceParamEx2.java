34116c08ca11ec829eeda21ca99691634f6324b1
daniel <bangseongmin@naver.com>
2024-02-15 01:09:05 +0900

    Chapter 06. 객체지향 프로그램 1

---
+++ src/chapter6/ReferenceParamEx2.java
@@ -0,0 +1,19 @@
+package chapter6;
+
+public class ReferenceParamEx2 {
+
+    public static void main(String[] args) {
+        int[] x = { 10 };
+        System.out.println("main() : x = " + x[0]);
+
+        change(x);
+        System.out.println("After change(d)");
+        System.out.println("main() : x = " + x[0]);
+    }
+
+    private static void change(int[] x) {
+        x[0] = 1000;
+        System.out.println("change() : x = " + x[0]);
+    }
+
+}
